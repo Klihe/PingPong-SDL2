f60b8b513b6fe15571fd33649236c4a6027914c5
Klihe <stepan.birke@gmail.com>
2024-02-23 08:51:54 +0100

    Ball - inc speed

--- src/ball.c
+++ src/ball.c
@@ -8,7 +8,7 @@ void drawBall(SDL_Renderer* renderer, Ball* ball) {
 void resetBall(Ball* ball, int rand) {
     ball->rect.x = WINDOW_WIDTH/2 - ball->rect.w/2;
     ball->rect.y = WINDOW_HEIGHT/2 - ball->rect.h/2;
-    ball->speed = 5; // 5 / 50
+    ball->speed = 50;
     ball->angle = rand % 360;
 }
 
@@ -17,19 +17,17 @@ void moveBall(Ball* ball, int rand) {
         resetBall(ball, rand);
     }
     else if (ball->rect.y < 0 || ball->rect.y > WINDOW_HEIGHT - ball->rect.h) {
-        ball->angle = -ball->angle - rand % 5;
-        // ball->speed += 2;
+        ball->angle = -ball->angle - rand % 10;
+        ball->speed += 2;
     }
-    // ball->speed /= 10;
-    ball->rect.x += (int)(ball->speed * cos((double)ball->angle * M_PI / 360));
-    ball->rect.y += (int)(ball->speed * sin((double)ball->angle * M_PI / 360));
-    // ball->speed *= 10;
+    ball->rect.x += (int)((ball->speed)/10 * cos((double)ball->angle * M_PI / 360));
+    ball->rect.y += (int)((ball->speed)/10 * sin((double)ball->angle * M_PI / 360));
 }
 
 bool checkCollisionBall(Ball* ball, SDL_Rect* rect1, SDL_Rect* rect2, int rand) {
     if (SDL_HasIntersection(&ball->rect, rect1) || SDL_HasIntersection(&ball->rect, rect2)) {
-        ball->angle = 360 -ball->angle - rand % 15;
-        // ball->speed = -ball->speed;
+        ball->angle = 360 -ball->angle - rand % 30;
+        ball->speed += 5;
         return true;
     }
     return false;
